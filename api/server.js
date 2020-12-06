ae7ebab32e25aa9f0ad9cefc044d6815fe40463e
Gustavo Souza <gtssouza94@gmail.com>
2020-12-06 18:13:35 -0300

    first commit

---
+++ api/server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = 8888;
+
+app.listen(port);
+
+console.log('O servidor http esta escutando na porta:' + port);
