065ab1ae2d160fe76ac559ec11c0ad0ceadb244e
Gustavo Souza <gtssouza94@gmail.com>
2020-12-07 09:44:23 -0300

    Conexão com o MongoDb e fazendo inserção de dados com post

--- api/server.js
+++ api/server.js
@@ -7,9 +7,15 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = 8888;
+var port = 8882;
 
 app.listen(port);
+
+var conn = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),
+    {}
+);
 
 console.log('O servidor http esta escutando na porta:' + port);
 
@@ -18,3 +24,21 @@ app.get('/', function(req, res){
         msg:'Ola'
     });
 })
+
+app.post('/api', function(req, res){
+    var dados = req.body;
+    res.send(dados);
+
+    conn.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.insert(dados, function(err, records){
+                if(err){
+                    res.json(err)
+                }else{
+                    res.json(records)
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
